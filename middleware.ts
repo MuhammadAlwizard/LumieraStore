c23464c96a6cf01b3dc319006a1ca37587a4a2ae
Muhammad Alwizard <alwizard659@gmail.com>
2026-09-14 23:54:13 +0700

    Protect admin routes with custom login

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,3 @@
-export { default } from 'next-auth/middleware';
-export const config = { matcher: ['/admin/:path*'] };
+import { withAuth } from 'next-auth/middleware';
+export default withAuth({ pages: { signIn: '/admin/login' } });
+export const config = { matcher: ['/admin/((?!login).*)'] };
